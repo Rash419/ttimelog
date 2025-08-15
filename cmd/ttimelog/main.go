5e316f6c382c270156ba54e3aa0f85d14fca637e
Rashesh Padia <rasheshpadia419@gmail.com>
2025-08-15 18:40:07 +0530

    intial commit
    - use lipgloss to draw borders and use textInput to get input

---
+++ cmd/ttimelog/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/Rash419/ttimelog/internal/layout"
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/x/ansi"
+)
+
+type model struct {
+	textInput textinput.Model
+	err       error
+	width     int
+	height    int
+	list      []string
+}
+
+type (
+	errMsg error
+)
+
+func initialModel() model {
+	txtInput := textinput.New()
+	txtInput.Focus()
+
+	return model{
+		textInput: txtInput,
+		err:       nil,
+		list:      make([]string, 0),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return tea.Batch(
+		tea.SetWindowTitle("Time log"),
+		textinput.Blink,
+	)
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		case tea.KeyEnter:
+			val := m.textInput.Value()
+			if val != "" {
+				m.list = append(m.list, val)
+				m.textInput.Reset()
+			}
+		}
+
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	border := layout.GetBorderStyle()
+
+	textinputBox := border.Width(m.width - 2).Render(
+		fmt.Sprintf("%v %s", time.Now().Format("15:04:05"), m.textInput.View()),
+	)
+	textInputHeight := lipgloss.Height(ansi.Strip(textinputBox))
+
+	listBoxHeight := max(m.height-textInputHeight, 1)
+
+	var listBoxData string
+	for _, item := range m.list {
+		listBoxData += fmt.Sprintf("* %s\n", item)
+	}
+	listBox := border.Width(m.width - 2).Height(listBoxHeight - 2).Render(listBoxData)
+
+	return lipgloss.JoinVertical(lipgloss.Left, listBox, textinputBox)
+}
+
+func main() {
+	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+}
